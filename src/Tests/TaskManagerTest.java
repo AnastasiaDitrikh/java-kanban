bc0bcdf18cfbf51bc57863aed9e5b45e87a805ea
DitrikhAnastasia <ditrihanastasya@yandex.ru>
2023-02-20 03:40:24 +0300

    7спринт

---
+++ src/Tests/TaskManagerTest.java
@@ -0,0 +1,92 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskManagerTest {
+
+    @Test
+    void getHistory() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void saveTask() {
+    }
+
+    @Test
+    void getListAllTask() {
+    }
+
+    @Test
+    void removeAllTask() {
+    }
+
+    @Test
+    void getTaskById() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void removeTaskById() {
+    }
+
+    @Test
+    void saveEpic() {
+    }
+
+    @Test
+    void getListAllEpic() {
+    }
+
+    @Test
+    void removeAllEpic() {
+    }
+
+    @Test
+    void getEpicById() {
+    }
+
+    @Test
+    void updateEpic() {
+    }
+
+    @Test
+    void removeEpicById() {
+    }
+
+    @Test
+    void saveSubtask() {
+    }
+
+    @Test
+    void getSubtaskById() {
+    }
+
+    @Test
+    void getSubtasks() {
+    }
+
+    @Test
+    void updateSubtask() {
+    }
+
+    @Test
+    void deleteSubtasks() {
+    }
+
+    @Test
+    void removeSubtaskById() {
+    }
+
+    @Test
+    void getEpicSubtasks() {
+    }
+}
